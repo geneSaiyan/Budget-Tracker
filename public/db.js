def08157c4c8de6a817a68b0b6d84c85a23412eb
Gene Edwards <eedwards@su.edu>
2020-03-31 21:31:37 -0400

    Added manifest link to index.html

---
+++ public/db.js
@@ -0,0 +1,57 @@
+let db_budget;
+
+const request = indexedDB.open("budget", 1);
+
+request.onupgradeneeded = ({ target }) => {
+  let db_budget = target.result;
+  db_budget.createObjectStore("pending", { autoIncrement: true });
+};
+
+request.onsuccess = ({ target }) => {
+  db_budget = target.result;
+
+  if (navigator.onLine) {
+    checkDatabase();
+  }
+};
+
+request.onerror = function(event) {
+  console.log("Error " + event.target.errorCode);
+};
+
+function saveRecord(record) {
+  const transaction = db_budget.transaction(["pending"], "readwrite");
+  const store = transaction.objectStore("pending");
+
+  store.add(record);
+}
+
+function checkDatabase() {
+  const transaction = db_budget.transaction(["pending"], "readwrite");
+  const store = transaction.objectStore("pending");
+  const getAll = store.getAll();
+
+  getAll.onsuccess = function() {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json"
+        }
+      })
+      .then(response => {        
+        return response.json();
+      })
+      .then(() => {
+        const transaction = db_budget.transaction(["pending"], "readwrite");
+        const store = transaction.objectStore("pending");
+        store.clear();
+      });
+    }
+  };
+}
+
+// check for app coming back online
+window.addEventListener("online", checkDatabase);
